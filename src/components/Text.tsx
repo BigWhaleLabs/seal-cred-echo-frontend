57e0c772e518ba10e9f7a3128738f107b39d1845
Tuan Dau <tukauu@gmail.com>
2022-07-12 12:06:29 -0400

    removed unused text styles

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -7,29 +7,10 @@ import {
   fontSize,
   fontWeight,
   lineHeight,
-  textAlign,
   textColor,
   textDecoration,
 } from 'classnames/tailwind'
 import ChildrenProp from 'models/ChildrenProp'
-
-const whiteText = classnames(textColor('text-white'))
-const grayText = classnames(textColor('text-gray-300'))
-
-const headerText = classnames(
-  whiteText,
-  fontSize('text-3xl', 'md:text-6xl'),
-  fontWeight('font-bold'),
-  textAlign('text-center')
-)
-export function HeaderText({ children }: ChildrenProp) {
-  return <p className={headerText}>{children}</p>
-}
-
-const bodyText = classnames(grayText, textAlign('text-center'))
-export function BodyText({ children }: ChildrenProp) {
-  return <p className={bodyText}>{children}</p>
-}
 
 const logoSubText = classnames(
   textColor('text-primary-semi-dimmed'),
