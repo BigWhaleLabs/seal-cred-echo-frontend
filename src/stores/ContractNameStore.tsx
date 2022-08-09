1bc124e698e9f3974ccf216cdb67981a86109f25
Ruslan Zapevalov <upacyxou@tutanota.com>
2022-08-09 22:32:35 +0400

    bump @bwl/stores and remove disallowList

--- src/stores/ContractNameStore.tsx
+++ src/stores/ContractNameStore.tsx
@@ -2,11 +2,7 @@ import { ContractNamesStore } from '@big-whale-labs/stores'
 import { proxy } from 'valtio'
 import env from 'helpers/env'
 
-export default proxy(
-  new ContractNamesStore([
-    'requestedNames',
-    'contractNames',
-    'requestedSymbols',
-    'contractSymbols',
-  ])
-).makePersistent(true, env.VITE_ENCRYPT_KEY)
+export default proxy(new ContractNamesStore()).makePersistent(
+  true,
+  env.VITE_ENCRYPT_KEY
+)
