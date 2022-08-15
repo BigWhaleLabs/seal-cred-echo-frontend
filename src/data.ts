c0a80e8384df88b2ca16b5f28e5b5aea4767c3f3
Michel Erokhov <mixaileroxov@gmail.com>
2022-08-15 18:23:08 +0300

    fix data.ts

--- src/data.ts
+++ src/data.ts
@@ -24,20 +24,4 @@ const data = {
   }
 }
 
-export default {
-  Email: {
-    ledger: env.VITE_SC_EMAIL_LEDGER_CONTRACT_ADDRESS,
-    postStorage: env.VITE_SC_EMAIL_POSTS_CONTRACT_ADDRESS,
-    twitter: 'SealCredEmail',
-  },
-  ERC721: {
-    ledger: env.VITE_SC_ERC721_LEDGER_CONTRACT_ADDRESS,
-    postStorage: env.VITE_SC_ERC721_POSTS_CONTRACT_ADDRESS,
-    twitter: 'SealCredGNFT',
-  },
-  ExternalERC721: {
-    ledger: env.VITE_SC_EXTERNAL_ERC721_LEDGER_CONTRACT_ADDRESS,
-    postStorage: env.VITE_SC_EXTERNAL_ERC721_POSTS_CONTRACT_ADDRESS,
-    twitter: 'SealCredNFT',
-  },
-}
+export default data
