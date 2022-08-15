9935e792c81430503b7827ed13872b2fbd07a7cd
smyzin <smyzin.work@gmail.com>
2022-08-15 14:36:14 +0300

    Fix infinity rerendering on typing tweet + disabled if no assets

--- src/components/CreatePost/Form.tsx
+++ src/components/CreatePost/Form.tsx
@@ -1,17 +1,10 @@
-import { HeaderText } from 'components/Text'
 import { useSnapshot } from 'valtio'
 import BottomPart from 'components/CreatePost/BottomPart'
 import DropDownStore from 'stores/DropDownStore'
-import NoBadgesMessage from 'components/CreatePost/NoBadgesMessage'
 import TextArea from 'components/TextArea'
 import TextStore from 'stores/TextStore'
 import classnames, { display, flexDirection, gap } from 'classnames/tailwind'
 
-const container = classnames(
-  display('flex'),
-  flexDirection('flex-col'),
-  gap('gap-y-6')
-)
 const formContainer = classnames(
   display('flex'),
   flexDirection('flex-col'),
@@ -25,26 +18,22 @@ export default function () {
   const maxLength = 280 - suffix.length
 
   return (
-    <div className={container}>
-      <HeaderText>Create an anonymous tweet</HeaderText>
-      <NoBadgesMessage />
-      <div className={formContainer}>
-        <TextArea
-          text={text}
-          placeholder={
-            selectedAddress
-              ? 'Share you anonymous message...'
-              : 'Select the asset to post as first!'
-          }
-          onTextChange={(newText) => {
-            TextStore.text = newText
-          }}
-          maxLength={maxLength}
-          suffix={suffix}
-          disabled={!selectedAddress}
-        />
-        <BottomPart />
-      </div>
+    <div className={formContainer}>
+      <TextArea
+        text={text}
+        placeholder={
+          selectedAddress
+            ? 'Share you anonymous message...'
+            : 'Select the asset to post as first!'
+        }
+        onTextChange={(newText) => {
+          TextStore.text = newText
+        }}
+        maxLength={maxLength}
+        suffix={suffix}
+        disabled={!selectedAddress}
+      />
+      <BottomPart />
     </div>
   )
 }
