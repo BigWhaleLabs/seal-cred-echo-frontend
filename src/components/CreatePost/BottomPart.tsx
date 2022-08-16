48cd2d0dba147f6b6925508ddb5d05bf4b1c5519
Daniil <dpancov64@gmail.com>
2022-08-16 13:01:35 +0300

    should have a 'select a zk badge' text

--- src/components/CreatePost/BottomPart.tsx
+++ src/components/CreatePost/BottomPart.tsx
@@ -1,33 +1,46 @@
+import { BodyText } from 'components/Text'
 import Button from 'components/Button'
 import SelectAsset from 'components/CreatePost/SelectAsset'
 import classnames, {
   alignItems,
   display,
+  flexDirection,
   flexWrap,
   gap,
   justifyContent,
+  width,
 } from 'classnames/tailwind'
 
 const container = classnames(
   display('flex'),
+  flexDirection('flex-col'),
+  gap('gap-y-2')
+)
+const bottomContainer = classnames(
+  display('flex'),
   justifyContent('justify-between'),
+  width('w-full'),
   alignItems('items-center'),
   flexWrap('flex-wrap'),
   gap('gap-y-4')
 )
+
 export default function () {
   return (
     <div className={container}>
-      <SelectAsset />
-      <Button
-        type="primary"
-        title="Tweet"
-        onClick={() => {
-          console.log('Tweet!')
-        }}
-      >
-        Tweet!
-      </Button>
+      <BodyText>Select a ZK Badge</BodyText>
+      <div className={bottomContainer}>
+        <SelectAsset />
+        <Button
+          type="primary"
+          title="Tweet"
+          onClick={() => {
+            console.log('Tweet!')
+          }}
+        >
+          Tweet!
+        </Button>
+      </div>
     </div>
   )
 }
