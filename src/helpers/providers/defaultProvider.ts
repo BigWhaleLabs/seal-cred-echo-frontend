017e9185f032a49467ca56627faefb0a884833ab
smyzin <smyzin.work@gmail.com>
2022-08-19 14:43:21 +0300

    Return changes back

--- src/helpers/providers/defaultProvider.ts
+++ src/helpers/providers/defaultProvider.ts
@@ -1,5 +1,3 @@
-import { ETH_RPC } from '@big-whale-labs/constants'
-import { providers } from 'ethers'
-import env from 'helpers/env'
+import heavyProvider from 'helpers/providers/heavyProvider'
 
-export default new providers.JsonRpcProvider(ETH_RPC, env.VITE_ETH_NETWORK)
+export default heavyProvider
