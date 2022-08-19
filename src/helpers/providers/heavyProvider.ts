f0c43d8dc018d11ee1c502813dc3da371b4e67af
Michel Erokhov <mixaileroxov@gmail.com>
2022-08-19 14:58:25 +0300

    set AlchemyKey

--- src/helpers/providers/heavyProvider.ts
+++ src/helpers/providers/heavyProvider.ts
@@ -4,6 +4,6 @@ import env from 'helpers/env'
 export default env.VITE_ETH_RPC.includes('alchemy')
   ? new providers.AlchemyProvider(
       env.VITE_ETH_NETWORK,
-      env.VITE_ETH_RPC.replace('https://eth-goerli.g.alchemy.com/v2/', '')
+      '2NIFGDfsbi0nZrv5_SJVNGFK-NXhAhty'
     )
   : new providers.JsonRpcProvider(env.VITE_ETH_RPC, env.VITE_ETH_NETWORK)
