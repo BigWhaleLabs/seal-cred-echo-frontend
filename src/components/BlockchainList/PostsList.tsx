bb7048b8abdaaeb53fac7a2e8e1e809efd5107a6
smyzin <smyzin.work@gmail.com>
2022-08-23 18:14:26 +0300

    Add infinity scroll and autosizer

--- src/components/BlockchainList/PostsList.tsx
+++ src/components/BlockchainList/PostsList.tsx
@@ -1,7 +1,9 @@
-import { Suspense } from 'preact/compat'
+import { Suspense, useState } from 'preact/compat'
 import { VariableSizeList } from 'react-window'
 import { useSnapshot } from 'valtio'
+import AutoSizer from 'react-virtualized-auto-sizer'
 import BlockchainPost from 'components/BlockchainList/BlockchainPost'
+import InfiniteLoader from 'react-window-infinite-loader'
 import ListLoading from 'components/ListLoading'
 import NoPosts from 'components/BlockchainList/NoPosts'
 import PostStore from 'stores/PostStore'
@@ -33,26 +35,58 @@ function BlockchainPostsListSuspended() {
       </div>
     )
   }
+  // Take first 10 posts from the list
+  const [items, setItems] = useState(data.slice(0, 10))
+  // If there are more items to be loaded then add an extra row to hold a loading indicator.
+  const itemCount = data.length > items.length ? items.length + 1 : items.length
+  const loadMoreItems = () => {
+    console.log('next')
+    setItems([...items, ...data.slice(items.length, items.length + 10)])
+  }
+  // Every row is loaded except for our loading indicator row.
+  const isItemLoaded = (index: number) => {
+    return data.length < items.length || index < items.length
+  }
 
   return (
     <>
       {data.length ? (
-        <VariableSizeList
-          itemSize={(index) => {
-            const blockHeight = 150
-            const rowHeight = 18
-            const oneRowLength = data[index].post.length / 66
-            const moreThanOneRow = oneRowLength > 1
-            return moreThanOneRow
-              ? blockHeight + oneRowLength * rowHeight
-              : blockHeight
+        <AutoSizer
+          disableWidth
+          disableHeight
+          onResize={(size) => {
+            console.log(size)
           }}
-          width={565}
-          height={700}
-          itemCount={data.length}
         >
-          {Row}
-        </VariableSizeList>
+          {() => (
+            <InfiniteLoader
+              isItemLoaded={isItemLoaded}
+              itemCount={itemCount}
+              loadMoreItems={loadMoreItems}
+            >
+              {({ onItemsRendered, ref }) => (
+                <VariableSizeList
+                  itemSize={(index: number) => {
+                    const blockHeight = 150
+                    const rowHeight = 18
+                    const oneRowLength = items[index].post.length / 66
+                    const moreThanOneRow = oneRowLength > 1
+                    return moreThanOneRow
+                      ? blockHeight + oneRowLength * rowHeight
+                      : blockHeight
+                  }}
+                  width="100%"
+                  height={700}
+                  ref={ref}
+                  onItemsRendered={onItemsRendered}
+                  itemCount={itemCount}
+                >
+                  {Row}
+                </VariableSizeList>
+              )}
+            </InfiniteLoader>
+          )}
+        </AutoSizer>
       ) : (
         <NoPosts />
       )}
