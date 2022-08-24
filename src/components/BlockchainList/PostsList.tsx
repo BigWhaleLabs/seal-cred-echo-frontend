04320a5b7343a74e66565b4ce118346e4dcbc602
smyzin <smyzin.work@gmail.com>
2022-08-24 13:28:45 +0300

    Updates according to comments

--- src/components/BlockchainList/PostsList.tsx
+++ src/components/BlockchainList/PostsList.tsx
@@ -1,4 +1,4 @@
-import { AccentText, LoadingText } from 'components/Text'
+import { LoadingText } from 'components/Text'
 import { Suspense, useState } from 'preact/compat'
 import { useSnapshot } from 'valtio'
 import BlockchainPost from 'components/BlockchainList/BlockchainPost'
@@ -6,24 +6,13 @@ import InfiniteScroll from 'react-infinite-scroll-component'
 import ListLoading from 'components/ListLoading'
 import NoPosts from 'components/BlockchainList/NoPosts'
 import PostStore from 'stores/PostStore'
-import SealLogo from 'icons/SealLogo'
-import classnames, {
-  alignItems,
-  display,
-  flexDirection,
-  gap,
-  margin,
-  width,
-} from 'classnames/tailwind'
+import classnames, { display, flexDirection, gap } from 'classnames/tailwind'
 import useScrollToAnchor from 'hooks/useScrollToAnchor'
 
-const endMessageWrapper = classnames(
+const scrollContainer = classnames(
   display('flex'),
   flexDirection('flex-col'),
-  alignItems('items-center'),
-  margin('my-4'),
-  gap('gap-y-2'),
-  width('w-full')
+  gap('gap-y-4')
 )
 
 function BlockchainPostsListSuspended() {
@@ -35,48 +24,39 @@ function BlockchainPostsListSuspended() {
       ? post.derivativeAddress === PostStore.selectedToken
       : post
   )
-  const EndMessage = () => (
-    <div className={endMessageWrapper}>
-      <SealLogo />
-      <AccentText bold color="text-accent">
-        No more posts
-      </AccentText>
-    </div>
-  )
   // Take first 10 posts from the list
   const [items, setItems] = useState(data.slice(0, 10))
-  // If there are more items to be loaded then add an extra row to hold a loading indicator.
+  const loadedItemsAmount = items.length
+  // If there are more items to be loaded then take data from the list
   const loadMoreItems = () => {
-    setItems([...items, ...data.slice(items.length, items.length + 10)])
+    setItems([
+      ...items,
+      ...data.slice(loadedItemsAmount, loadedItemsAmount + 10),
+    ])
   }
 
-  return (
-    <>
-      {data.length ? (
-        <InfiniteScroll
-          dataLength={items.length}
-          next={loadMoreItems}
-          hasMore={items.length < data.length}
-          loader={<LoadingText>Fetching more posts...</LoadingText>}
-          endMessage={<EndMessage />}
-        >
-          <>
-            {items.map((post) => (
-              <BlockchainPost
-                key={post.id}
-                id={Number(post.id)}
-                timestamp={Number(post.timestamp)}
-                text={post.post}
-                sender={post.sender}
-                derivativeAddress={post.derivativeAddress}
-              />
-            ))}
-          </>
-        </InfiniteScroll>
-      ) : (
-        <NoPosts />
-      )}
-    </>
+  return data.length ? (
+    <InfiniteScroll
+      endMessage={<></>}
+      next={loadMoreItems}
+      className={scrollContainer}
+      dataLength={loadedItemsAmount}
+      hasMore={loadedItemsAmount < data.length}
+      loader={<LoadingText>Fetching more posts...</LoadingText>}
+    >
+      {items.map((post) => (
+        <BlockchainPost
+          key={post.id}
+          id={Number(post.id)}
+          timestamp={Number(post.timestamp)}
+          text={post.post}
+          sender={post.sender}
+          derivativeAddress={post.derivativeAddress}
+        />
+      ))}
+    </InfiniteScroll>
+  ) : (
+    <NoPosts />
   )
 }
 
